de6f3f5307313fb7e0fa77621674d3158c431970
SerjMogilat <ABCser@ya.ru>
2023-03-04 14:42:38 +0300

    обавил задачи в 1 семинар

--- Java/Seminars/sem1/task1.java
+++ Java/Seminars/sem1/task1.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 public class task1 {
     public static void main(String[] args) {
         Scanner name = new Scanner(System.in);
-        System.out.println(("Name is "));
+        System.out.println(("What is your name?"));
         String name1 = name.nextLine();
+        System.out.println("Hello, " + name1 + "!");
         name.close();
-        System.out.println("Hello, " + name);
     }
 }
-
