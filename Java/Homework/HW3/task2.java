bef7026f04aefb587094dc052f6d327a6fcc1afe
SerjMogilat <ABCser@ya.ru>
2023-03-13 13:17:11 +0300

    Решены 1 и 2 задачи 3 семинара

--- Java/Homework/HW3/task2.java
+++ Java/Homework/HW3/task2.java
@@ -2,5 +2,23 @@ package Homework.HW3;
 /*
 Пусть дан произвольный список целых чисел, удалить из него чётные числа
  */
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class task2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<>(20);
+        for (int i = 0; i < 20; i++) {
+            myList.add(new Random().nextInt(0, 100));
+        }
+        System.out.println(myList);
+        for (int i = myList.size() - 1; i >= 0; i--) {
+            if (myList.get(i) % 2 == 0) {
+                myList.remove(i);
+            }
+        }
+        System.out.println(myList);
+    }
+
 }
