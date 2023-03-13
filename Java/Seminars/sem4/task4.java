f584e468207ce651d22b8b8dbbd0b949025222b1
SerjMogilat <ABCser@ya.ru>
2023-03-13 13:11:40 +0300

    4 семинар, работа в группах

---
+++ Java/Seminars/sem4/task4.java
@@ -0,0 +1,19 @@
+package Seminars.sem4;
+import java.util.Stack;
+
+/*
+Написать метод, который принимает массив элементов, помещает их в стэк и выводит на консоль содержимое стэка.
+ */
+public class task4 {
+    public static void main(String[] args) {
+        int[] myArray = {1, 2, 3, 4, 5, 6, 7};
+        Stack<Integer> myStack = new Stack<>();
+        for (int i : myArray) {
+            myStack.push(i);
+        }
+        System.out.println(myStack);
+        while (!myStack.isEmpty()) {
+            System.out.println(myStack.pop());
+        }
+    }
+}
